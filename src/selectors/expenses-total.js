8d4506ab35946716693af98b0c5d01a96cea4507
Mark Douglas Foti <markfoti@Marks-MacBook-Air.local>
2018-10-19 13:24:05 -0700

    Add expense summary to dashboard

---
+++ src/selectors/expenses-total.js
@@ -0,0 +1,21 @@
+import expenses from "../tests/fixtures/expenses";
+
+export default (expenses) => {
+    if (expenses.length === 0) {
+        return 0;
+    } else {
+        return expenses
+            .map((expense) => expense.amount)
+            .reduce((sum, value) => {
+                return sum + value;
+            }, 0);
+    }
+};
+
+
+
+// const getExpensesTotal = (expenses, prop) => {
+//     return expenses.reduce( function(prev, next) {
+//         return prev + next[prop];
+//     }, 0);
+// };
